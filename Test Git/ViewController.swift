ce49ce7803fe05dc46209bd78b1c199b20b066c9
Matias Mansilla <dmatiasmansilla@gmail.com>
2020-04-25 00:13:19 -0300

    Agregado un segundo botón de impresión

--- Test Git/ViewController.swift
+++ Test Git/ViewController.swift
@@ -7,8 +7,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginLabel :UILabel?
    
     @IBAction func goToTapped(_ sender: UIButton) {
-        print("otsea Helloo ")
+        print("Insertar algo aca")
     }
+    
+    @IBAction func printTapped(_ sender: Any) {
+        print("Otra cosa")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("test para probar Git")
