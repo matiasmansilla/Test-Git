caff102908dae6bae67097613f7418b56e819fc7
Matias Mansilla <dmatiasmansilla@gmail.com>
2020-04-25 00:03:27 -0300

    ahora buttonTapped imprime una fecha

--- Test Git/ViewController.swift
+++ Test Git/ViewController.swift
@@ -7,10 +7,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginLabel :UILabel?
    
     @IBAction func goToTapped(_ sender: UIButton) {
-        print("otsea Helloo ")
+        let today = NSDate.self
+        print(today)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         print("test para probar Git")
         // Do any additional setup after loading the view.
     }
