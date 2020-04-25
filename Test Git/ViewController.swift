55ed95f42cde64c59fe508f3932ee6ba082f7122
Matias Mansilla <dmatiasmansilla@gmail.com>
2020-04-24 23:51:22 -0300

    agregar botón de funcionalidad

--- Test Git/ViewController.swift
+++ Test Git/ViewController.swift
@@ -1,17 +1,17 @@
-//
-//  ViewController.swift
-//  Test Git
-//
-//  Created by Dardo Matias Mansilla on 24/04/2020.
-//  Copyright © 2020 Dardo Matias Mansilla. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginLabel :UILabel?
+   
+    @IBAction func goToTapped(_ sender: UIButton) {
+        print("otsea Helloo ")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("test para probar Git")
         // Do any additional setup after loading the view.
     }
 
